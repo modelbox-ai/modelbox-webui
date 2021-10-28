3e6d9cae70bf75d0efece9d4a89dcb584504e7ec
dengweijie <dengweijie5@huawei.com>
2021-10-28 10:51:21 +0800

    UI: formate code and add copyright info

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -1,4 +1,21 @@
-/* eslint-disable */
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {
   Component,
   ViewChild,
@@ -1048,15 +1065,15 @@ export class GraphComponent implements AfterViewInit, OnChanges {
     this.selectComponents(d3_select(nodes[i]));
   }
 
-  isSameBbox(bbox){
-    if (this.nodeBbox === undefined){
+  isSameBbox(bbox) {
+    if (this.nodeBbox === undefined) {
       this.nodeBbox = bbox;
-    }else{
+    } else {
       if (this.nodeBbox.height === bbox.height &&
-        this.nodeBbox.x === bbox.x && 
-        this.nodeBbox.y === bbox.y){
-          bbox = this.nodeBbox
-        }
+        this.nodeBbox.x === bbox.x &&
+        this.nodeBbox.y === bbox.y) {
+        bbox = this.nodeBbox
+      }
     }
     return bbox;
   }
