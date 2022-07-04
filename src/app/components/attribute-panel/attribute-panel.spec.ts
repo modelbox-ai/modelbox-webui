fe5a6fe42b726ecd8234a3d34d74721ad25e53dc
ModelboxDENG <dwjandzzs@gmail.com>
2022-07-04 16:27:06 +0800

    UI: add coverage

--- src/app/components/attribute-panel/attribute-panel.spec.ts
+++ src/app/components/attribute-panel/attribute-panel.spec.ts
@@ -7,7 +7,7 @@ import { DataServiceService } from "@shared/services/data-service.service";
 import { OverlayContainerModule, OverlayContainerRef } from "ng-devui/overlay-container";
 import { AttributePanelComponent } from "./attribute-panel.component";
 import { DialogService } from "ng-devui";
-import { context_origin, context_target, unit_target, config, unit_http_receive } from "./mock-data";
+import { context_origin, context_target, unit_target, config, unit_http_receive, unit_type_target, config_initialed } from "./mock-data";
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, '../../../i18n/', '.json');
@@ -64,6 +64,27 @@ describe("AttributePanelComponent", () => {
     expect(app.unit.inputports).toBeFalsy();
     expect(app.unit.options).toBeFalsy();
     expect(app.unit.outputports).toBeFalsy();
+    app.unitType.init();
+    app.unitOptions.init();
+    expect(app.unitType.options).toEqual(unit_type_target.options);
+    expect(app.unitType.selected).toEqual(unit_type_target.selected);
+    expect(app.unitOptions.data.length).toEqual(0);
+  });
+
+  it('initConfig', () => {
+    const fixture = TestBed.createComponent(AttributePanelComponent);
+    const app = fixture.componentInstance;
+    app.config = config;
+    app.initConfig(config);
+    expect(app.newName).toEqual("httpserver_sync_receive");
+    expect(app.config["attributes"]).toEqual(config_initialed["attributes"]);
+  });
+
+  it('getUnit', () => {
+    const fixture = TestBed.createComponent(AttributePanelComponent);
+    const app = fixture.componentInstance;
+    app.config = config_initialed;
+    expect(app.getUnit(config_initialed)).toEqual(unit_http_receive);
   });
 
 });
