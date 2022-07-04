0e5ce833dd54cd2e85bffd702bc5f2502c88114f
ModelboxDENG <dwjandzzs@gmail.com>
2022-07-04 16:27:06 +0800

    UI: add coverage

--- src/app/components/attribute-panel/mock-data.ts
+++ src/app/components/attribute-panel/mock-data.ts
@@ -304,3 +304,144 @@ export let config = {
   ],
   "name": "httpserver_sync_receive"
 }
+
+export let unit_type_target = {
+
+  "options": [
+    {
+      "id": "cpu",
+      "label": "cpu"
+    }
+  ],
+  "selected": {
+    "id": "cpu",
+    "label": "cpu"
+  }
+}
+
+export let unit_options_data_target = [
+  {
+    "label": "endpoint",
+    "required": true,
+    "type": "string",
+    "default": "https://127.0.0.1:8080",
+    "desc": "http server listen URL.",
+    "value": "https://127.0.0.1:8080"
+  },
+  {
+    "label": "max requests",
+    "required": false,
+    "type": "integer",
+    "default": "1000",
+    "desc": "max http request.",
+    "value": "1000"
+  },
+  {
+    "label": "keepalive timeout sec",
+    "required": false,
+    "type": "integer",
+    "default": "200",
+    "desc": "keep-alive timeout time(sec)",
+    "value": "200"
+  },
+  {
+    "label": "time out ms",
+    "required": false,
+    "type": "integer",
+    "default": "5000",
+    "desc": "max http request timeout. ",
+    "value": "5000"
+  },
+  {
+    "label": "cert",
+    "required": false,
+    "type": "string",
+    "default": "",
+    "desc": "cert file path",
+    "value": ""
+  },
+  {
+    "label": "key",
+    "required": false,
+    "type": "string",
+    "default": "",
+    "desc": "key file path",
+    "value": ""
+  },
+  {
+    "label": "passwd",
+    "required": false,
+    "type": "string",
+    "default": "",
+    "desc": "encrypted key file password.",
+    "value": ""
+  },
+  {
+    "label": "key pass",
+    "required": false,
+    "type": "string",
+    "default": "",
+    "desc": "key for encrypted password.",
+    "value": ""
+  }
+]
+
+export let config_initialed = {
+  "attributes": [
+    {
+      "key": "type",
+      "value": "flowunit"
+    },
+    {
+      "key": "flowunit",
+      "value": "httpserver_sync_receive"
+    },
+    {
+      "key": "device",
+      "value": "cpu"
+    },
+    {
+      "key": "time_out_ms",
+      "value": "5000"
+    },
+    {
+      "key": "endpoint",
+      "value": "http://0.0.0.0:7770"
+    },
+    {
+      "key": "max_requests",
+      "value": "100"
+    },
+    {
+      "key": "label",
+      "value": "{httpserver_sync_receive|{<out_request_info> out_request_info}}"
+    }
+  ],
+  "locations": [
+    {
+      "start": {
+        "offset": 60,
+        "line": 3,
+        "column": 5
+      },
+      "end": {
+        "offset": 286,
+        "line": 3,
+        "column": 231
+      }
+    },
+    {
+      "start": {
+        "offset": 572,
+        "line": 7,
+        "column": 5
+      },
+      "end": {
+        "offset": 612,
+        "line": 7,
+        "column": 45
+      }
+    }
+  ],
+  "name": "httpserver_sync_receive"
+}
