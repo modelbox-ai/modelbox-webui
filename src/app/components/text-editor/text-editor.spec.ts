26ac9fc4f8b95d702010a295faf8fda2daae9f1a
ModelBoxDENG <dwjandzzs@gmail.com>
2022-07-11 16:58:18 +0800

    UI: add texteditor unittest

--- src/app/components/text-editor/text-editor.spec.ts
+++ src/app/components/text-editor/text-editor.spec.ts
@@ -6,6 +6,8 @@ import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { DataServiceService } from "@shared/services/data-service.service";
 import { OverlayContainerModule, OverlayContainerRef } from "ng-devui/overlay-container";
 import { TextEditorComponent } from "./text-editor.component";
+import { AceComponent } from 'ngx-ace-wrapper';
+import { dotSrc } from "./mock-data";
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, '../../../i18n/', '.json');
@@ -13,10 +15,27 @@ export function createTranslateLoader(http: HttpClient) {
 
 describe("TextEditorComponent", () => {
 
+  const componentRef = jasmine.createSpyObj('AceComponent', {}, {
+    directiveRef: {
+      ace: (function () {
+        return {
+          container: {
+            style: {
+              lineHeight: 0
+            }
+          },
+          setOptions: new Function()
+        }
+      })
+    },
+
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        TextEditorComponent
+        TextEditorComponent,
+        AceComponent
       ],
       imports: [
         TranslateModule.forChild({
@@ -43,7 +62,37 @@ describe("TextEditorComponent", () => {
   it('should create the TextEditorComponent', () => {
     const fixture = TestBed.createComponent(TextEditorComponent);
     const app = fixture.componentInstance;
+
+    app.componentRef = componentRef;
+    app.registerUndo = (function () { });
+    app.registerRedo = (function () { });
+    app.registerUndoReset = (function () { });
+    app.registerResize = (function () { });
+    app.registerSwitchDirectionButtonClick = (function () { });
+
+    app.ngAfterViewInit();
     expect(app).toBeTruthy();
+    expect(app.editor).toBeTruthy();
+    expect(app.editor.container.style.lineHeight).toEqual(1.8);
+
+    expect(app.undo).toBeTruthy();
+    expect(app.redo).toBeTruthy();
+    expect(app.resetUndoStack).toBeTruthy();
+    expect(app.resize).toBeTruthy();
+    expect(app.switchDirection).toBeTruthy();
+    expect(app.config).toBeTruthy();
+    expect(app.config.printMarginColumn).toEqual(120);
   });
 
+  it('should switchComponent', () => {
+    const fixture = TestBed.createComponent(TextEditorComponent);
+    const app = fixture.componentInstance;
+    app.dotSrc = dotSrc;
+    app.switchDirection();
+    expect(app.dotSrc.indexOf('rankdir=LR')).toBeGreaterThan(-1);
+    app.switchDirection();
+    expect(app.dotSrc.indexOf('rankdir=VERTICLE')).toBeGreaterThan(-1);
+    app.switchDirection();
+    expect(app.dotSrc.indexOf('rankdir=LR')).toBeGreaterThan(-1);
+  });
 });
