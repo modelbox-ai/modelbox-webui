fef95a267e786963e1ed1efb299666a8a3edc964
ModelBoxDENG <dwjandzzs@gmail.com>
2022-07-21 16:49:01 +0800

    UI: add basic-service test

--- src/app/shared/services/basic-service.spec.ts
+++ src/app/shared/services/basic-service.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse, HttpParams, HttpRequest } from '@angular/common/http';
 import { BasicServiceService } from './basic-service.service';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { solutionList, solutionListTarget, loadTreeByPath } from "./mock-data";
+import {
+  solutionList,
+  solutionListTarget,
+  loadTreeByPath,
+  templateTarget,
+  paramQueryData,
+  flowInfo,
+  tasks,
+  demoTarget,
+  paramCreateProject,
+  paramCreateFlowunit,
+  paramCreateTask,
+  openProjectData,
+} from "./mock-data";
 
 describe('BasicServiceService', () => {
 
@@ -36,16 +49,20 @@ describe('BasicServiceService', () => {
     let response = "/root";
     service.queryRootPath().subscribe(data => {
       expect(data).toBeTruthy();
+      fail
     });
 
     // Expect a call to this URL
     const req = httpTestingController.expectOne(
-      "/editor/basic-info"
+      (request) => request.url === "/editor/basic-info"
     );
-    // Assert that the request is a GET.
+
     expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
     // Respond with this data when called
     req.flush(response);
+    httpTestingController.verify();
 
   });
 
@@ -53,35 +70,350 @@ describe('BasicServiceService', () => {
     let response = loadTreeByPath;
     service.loadTreeByPath().subscribe(data => {
       expect(data).toEqual(loadTreeByPath);
+      fail
     });
 
     // Expect a call to this URL
     const req = httpTestingController.expectOne(
-      "/editor/project/list/?path=/"
+      (request) => request.url === "/editor/project/list/?path=/"
     );
     // Assert that the request is a GET.
     expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
     // Respond with this data when called
     req.flush(response);
+    httpTestingController.verify();
 
   });
 
   it('querySolutionList', () => {
     let response = solutionList;
     service.querySolutionList().subscribe(data => {
-
       expect(data.demo_list).toEqual(solutionListTarget);
+      fail
     });
 
     // Expect a call to this URL
     const req = httpTestingController.expectOne(
-      "/editor/demo"
+      (request) => request.url === "/editor/demo"
     );
     // Assert that the request is a GET.
     expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
     // Respond with this data when called
     req.flush(response);
+    httpTestingController.verify();
 
   });
+
+  it('queryTemplate', () => {
+    let response = templateTarget;
+    service.queryTemplate().subscribe(data => {
+      expect(data.project_template_list).toEqual(templateTarget.project_template_list);
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/editor/project/template"
+    );
+    // Assert that the request is a GET.
+    expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('queryData', () => {
+    let response = flowInfo;
+    let param = new HttpParams({ fromObject: paramQueryData });
+    service.queryData({ params: param }).subscribe(data => {
+      expect(data.devices).toBeTruthy();
+      expect(data.flowunits).toBeTruthy();
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/editor/flow-info"
+    );
+
+    expect(req.request.method).toEqual("PUT");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.body.params).toEqual(param);
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('getTaskLists', () => {
+    let response = tasks;
+    service.getTaskLists().subscribe(data => {
+      expect(data.job_list).toEqual(tasks.job_list);
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      "/v1/modelbox/job/list/all"
+    );
+
+    expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('deleteTask', () => {
+    let param = "hello_world_diagraph.toml"
+    service.deleteTask(param).subscribe(data => {
+      expect(data).toBeFalsy();
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      "/v1/modelbox/job/list/" + param
+    );
+
+    expect(req.request.method).toEqual("DELETE");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush('');
+    httpTestingController.verify();
+
+  });
+
+  it('querySolution', () => {
+    let param = "hello_world_diagraph/hello_world_diagraph.toml"
+    let response = demoTarget;
+    service.querySolution(param).subscribe(data => {
+      expect(data).toEqual(demoTarget);
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/editor/demo/" + param
+    );
+    expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('searchTask', () => {
+    let param = "hello_world_diagraph.toml"
+    let response = tasks.job_list[0];
+    service.searchTask(param).subscribe(data => {
+      expect(data).toEqual(response);
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      "/v1/modelbox/job/" + param
+    );
+
+    expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('createTask', () => {
+    let response = {
+      code: "Success",
+      msg: "Success"
+    };
+    let paramTask = JSON.stringify(paramCreateTask)
+    let param = new HttpParams({ fromString: paramTask });
+    service.createTask({ params: param }).subscribe(data => {
+      expect(data?.body.code).toBeTruthy();
+      expect(data?.body.msg).toBeTruthy();
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/v1/modelbox/job"
+    );
+
+    expect(req.request.method).toEqual("PUT");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.body.params).toEqual(param);
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('saveAllProject', () => {
+    let response = {
+      code: "Success",
+      msg: "Success"
+    };
+    let paramTask = JSON.stringify(paramCreateTask)
+    let param = new HttpParams({ fromString: paramTask });
+    service.saveAllProject({ params: param }).subscribe(data => {
+      expect(data?.body.code).toBeTruthy();
+      expect(data?.body.msg).toBeTruthy();
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/editor/graph"
+    );
+
+    expect(req.request.method).toEqual("PUT");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.body.params).toEqual(param);
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('createProject', () => {
+    let response = {
+      code: "Success",
+      msg: "Success"
+    };
+    let param = new HttpParams({ fromObject: paramCreateProject });
+    service.createProject({ params: param }).subscribe(data => {
+      expect(data?.body.code).toBeTruthy();
+      expect(data?.body.msg).toBeTruthy();
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/editor/project/create"
+    );
+
+    expect(req.request.method).toEqual("PUT");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.body.params).toEqual(param);
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('createFlowunit', () => {
+    let response = {
+      code: "Success",
+      msg: "Success"
+    };
+    let param = new HttpParams({ fromObject: paramCreateFlowunit })
+      .append("input", "[{'name': 'input1','device': 'cpu'}]");
+    service.createFlowunit({ params: param }).subscribe(data => {
+      expect(data?.body.code).toBeTruthy();
+      expect(data?.body.msg).toBeTruthy();
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/editor/flowunit/create"
+    );
+
+    expect(req.request.method).toEqual("PUT");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.body.params).toEqual(param);
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('openProject', () => {
+    let param = "/root/projectName"
+    let response = openProjectData;
+    service.openProject(param).subscribe(data => {
+      expect(data).toEqual(response);
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      "/editor/project/?path=" + param
+    );
+
+    expect(req.request.method).toEqual("GET");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('customRequest', () => {
+    let response = {
+      code: "Success",
+      msg: "Success"
+    };
+    let method = "POST";
+    let route = "http://0.0.0.0:8190/v1/mnist_test";
+    let pram = "\"{\\n  \\\"image_base64\\\": \\\"\\\"\\n}\\n\"";
+    ;
+    service.customRequest(method, route, pram).subscribe(data => {
+      expect(data).toBeTruthy();
+      fail
+    });
+
+    // Expect a call to this URL
+    const req = httpTestingController.expectOne(
+      (request) => request.url === "/editor/postman"
+    );
+
+    expect(req.request.method).toEqual("POST");
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.body).toBeTruthy();
+    expect(req.request.responseType).toEqual('json');
+    // Respond with this data when called
+    req.flush(response);
+    httpTestingController.verify();
+
+  });
+
+  it('httpclienr 404 error test case', () => {
+    const errormsg = 'mock 404 error';
+    service.queryRootPath().subscribe((get) => {
+      fail('fail with error 404');
+    },
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(404);
+        expect(error.error).toEqual(errormsg);
+      });
+
+    const req = httpTestingController.expectOne('/editor/basic-info');
+    req.flush(errormsg, { status: 404, statusText: 'Not Found' });
+    httpTestingController.verify();
+
+  })
 
 });
--- src/app/shared/services/mock-data.ts
+++ src/app/shared/services/mock-data.ts
@@ -80,3 +80,277 @@ export let loadTreeByPath = {
     }
   ]
 }
+
+export let templateTarget = {
+  "project_template_list":
+    [
+      {
+        "desc": "A mnist with mindspore example project template for modelbox",
+        "dirname": "mnist-mindspore",
+        "name": "mnist-mindspore",
+        "restapi": {
+          "method": "POST",
+          "path": "http://0.0.0.0:8190/v1/mnist_test",
+          "requestbody": "{\n  \"image_base64\": \"\"\n}\n"
+        }
+      },
+      {
+        "desc": "A car detection example project template for modelbox",
+        "dirname": "car_detection",
+        "name": "car detection"
+      },
+      {
+        "desc": "A emotion detection example project template for modelbox",
+        "dirname": "emotion_detection",
+        "guide": {
+          "guide": "\n# 使用指导\n\n需要执行工程根目录下download_emotion_files.sh下载torch模型。\n\n"
+        },
+        "name": "emotion_detection"
+      },
+      {
+        "desc": "A resize example project template for modelbox",
+        "dirname": "resize",
+        "name": "resize"
+      },
+      {
+        "desc": "A helloworld REST API service example project template for modelbox",
+        "dirname": "hello_world",
+        "name": "helloworld",
+        "restapi": {
+          "method": "POST",
+          "path": "http://0.0.0.0:7770/v1/hello_world",
+          "requestbody": "{\n  \"msg\": \"hello modelbox\"\n}\n"
+        }
+      },
+      {
+        "desc": "A empty project template for modelbox",
+        "dirname": "empty",
+        "name": "empty"
+      },
+      {
+        "desc": "A mnist example project template for modelbox",
+        "dirname": "mnist",
+        "name": "mnist",
+        "restapi": {
+          "method": "POST",
+          "path": "http://0.0.0.0:8190/v1/mnist_test",
+          "requestbody": "{\n  \"image_base64\": \"\"\n}\n"
+        }
+      }
+    ]
+}
+
+export let paramQueryData = {
+  "dir": ["/root/face_detection/src/flowunit"],
+  "skip-default": false
+}
+
+export let flowInfo = {
+  devices: [
+    {
+      "descryption": "Host cpu device.",
+      "name": "0",
+      "type": "CPU",
+      "version": ""
+    }
+  ],
+  flowunits: [
+    {
+      "descryption": "\n\t@Brief: Modify the input buffer meta field name and value according to custom rules. \n\t@Port parameter: The input port and the output buffer type are binary. \n\t@Constraint: ",
+      "group": "Image",
+      "inputports": [
+        {
+          "name": "in_data"
+        }
+      ],
+      "name": "buff_meta_mapping",
+      "options": [
+        {
+          "default": "",
+          "desc": "the source meta",
+          "name": "src_meta",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "default": "",
+          "desc": "the dest meta",
+          "name": "dest_meta",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "default": "",
+          "desc": "the meta mapping rules",
+          "name": "rules",
+          "required": false,
+          "type": "string"
+        }
+      ],
+      "outputports": [
+        {
+          "name": "out_data"
+        }
+      ],
+      "type": "cpu",
+      "version": "1.0.0",
+      "virtual": false
+    }
+  ]
+}
+
+export let tasks = {
+  job_list:
+    [
+      {
+        "job_error_msg": "Not found, build graph from config fail. -> build node failed. name: 'hello_world' -> create flowunit 'hello_world' failed. -> can not find flowunit [type: cpu, name:hello_world], Please check if the 'device' configured correctly or if the flowunit library exists.",
+        "job_id": "hello_world_diagraph.toml",
+        "job_status": "FAILED"
+      }
+    ]
+}
+
+export let demoTarget = {
+  "graph": {
+    "graphconf": "digraph car_detection {\n    node [shape=Mrecord]\n    video_input[type=flowunit, flowunit=video_input, device=cpu, deviceid=0, source_url=\"/opt/modelbox/demo/video/car_test_video.mp4\"]\n    videodemuxer[type=flowunit, flowunit=video_demuxer, device=cpu, deviceid=0]\n    videodecoder[type=flowunit, flowunit=video_decoder, device=cuda, deviceid=0, pix_fmt=bgr]\n    image_resize[type=flowunit, flowunit=resize, device=cpu, deviceid=0, image_width=512, image_height=288]\n    image_transpose[type=flowunit, flowunit=packed_planar_transpose, device=cpu, deviceid=0]\n    normalize[type=flowunit, flowunit=normalize, device=cpu, deviceid=0, standard_deviation_inverse=\"1,1,1\"]\n    model_inference[type=flowunit, flowunit=car_detect, device=cuda, deviceid=0, batch_size=1]\n    yolox_post[type=flowunit, flowunit=yolox_post, device=cpu, deviceid=0]\n    videoencoder[type=flowunit, flowunit=video_encoder, device=cpu, deviceid=0, encoder=mpeg4, format=mp4, default_dest_url=\"/tmp/car_detection_result.mp4\"]\n\n    video_input:out_video_url -> videodemuxer:in_video_url\n    videodemuxer:out_video_packet -> videodecoder:in_video_packet\n    videodecoder:out_video_frame -> image_resize:in_image\n    image_resize:out_image -> image_transpose:in_image\n    image_transpose:out_image -> normalize:in_data\n    normalize:out_data -> model_inference:input\n    model_inference:output -> yolox_post:in_feat\n    videodecoder:out_video_frame -> yolox_post:in_image\n    yolox_post:out_data -> videoencoder:in_video_frame\n}",
+    "format": "graphviz"
+  },
+  "driver": {
+    "dir": [
+      "/usr/local/share/modelbox/demo/car_detection/flowunit"
+    ]
+  },
+  "flow": {
+    "desc": "car detection for video streams"
+  }
+}
+
+export let paramCreateProject = {
+  "name": "testProject",
+  "rootpath": "/root",
+  "template": "hello_world"
+}
+
+export let paramCreateFlowunit = {
+  "name": "flowunit",
+  "desc": "",
+  "lang": "python",
+  "project-path": "/root/projectName",
+  "device": "cpu",
+  "type": "stream",
+  "group-type": "generic"
+}
+
+export let paramCreateTask = {
+  "job_id": "graph_ddd.toml",
+  "graph_name": "graph_ddd.toml",
+  "graph": {
+    "flow": {
+      "desc": "A resize modelbox project"
+    },
+    "driver": {
+      "dir": "/root/ddd/src/flowunit"
+    },
+    "profile": {
+      "profile": false,
+      "trace": false,
+      "session": false,
+      "dir": "/tmp/modelbox/perf/"
+    },
+    "graph": {
+      "graphconf": "digraph graph_ddd {\n\tnode [shape=Mrecord]\n  video_input[type=flowunit, flowunit=video_input, device=cpu, deviceid=0, repeat=20, source_url=\"/xxx/xxx.mp4\"]\n  videodemuxer[type=flowunit, flowunit=video_demuxer, device=cpu, deviceid=0, queue_size_event=1000, ] \n  videodecoder[type=flowunit, flowunit=video_decoder, device=cuda, deviceid=0, pix_fmt=\"nv12\"]\n  output1[type=output]  \n  video_input:out_video_url -> videodemuxer:in_video_url\n  videodemuxer:out_video_packet -> videodecoder:in_video_packet\n  videodecoder:out_video_frame -> output1\n}",
+      "format": "graphviz"
+    }
+  },
+  "job_graph": {
+    "flow": {
+      "desc": "A resize modelbox project"
+    },
+    "driver": {
+      "dir": "/root/ddd/src/flowunit"
+    },
+    "profile": {
+      "profile": false,
+      "trace": false,
+      "session": false,
+      "dir": "/tmp/modelbox/perf/"
+    },
+    "graph": {
+      "graphconf": "digraph graph_ddd {\n\tnode [shape=Mrecord]\n  video_input[type=flowunit, flowunit=video_input, device=cpu, deviceid=0, repeat=20, source_url=\"/xxx/xxx.mp4\"]\n  videodemuxer[type=flowunit, flowunit=video_demuxer, device=cpu, deviceid=0, queue_size_event=1000, ] \n  videodecoder[type=flowunit, flowunit=video_decoder, device=cuda, deviceid=0, pix_fmt=\"nv12\"]\n  output1[type=output]  \n  video_input:out_video_url -> videodemuxer:in_video_url\n  videodemuxer:out_video_packet -> videodecoder:in_video_packet\n  videodecoder:out_video_frame -> output1\n}",
+      "format": "graphviz"
+    }
+  }
+}
+
+export let openProjectData = {
+  "flowunits": [
+    {
+      "base": {
+        "description": "A flowunit for modelbox",
+        "device": "cpu",
+        "entry": "flowunit@FlowunitFlowUnit",
+        "group_type": "generic",
+        "name": "flowunit",
+        "stream": true,
+        "type": "python",
+        "version": "1.0.0"
+      },
+      "input": {
+        "input1": {
+          "device": "cpu",
+          "name": "input1"
+        }
+      },
+      "output": {
+        "output1": {
+          "name": "output1"
+        }
+      }
+    }
+  ],
+  "graphs": [
+    {
+      "driver": {
+        "dir": [
+          "/opt/modelbox/application/ddd/flowunit"
+        ],
+        "skip-default": false
+      },
+      "flow": {
+        "desc": "A resize modelbox project"
+      },
+      "graph": {
+        "format": "graphviz",
+        "graphconf": "digraph graph_ddd {\n  video_input[type=flowunit, flowunit=video_input, device=cpu, deviceid=0, repeat=20, source_url=\"/xxx/xxx.mp4\"]\n  videodemuxer[type=flowunit, flowunit=video_demuxer, device=cpu, deviceid=0, queue_size_event=1000, ] \n  videodecoder[type=flowunit, flowunit=video_decoder, device=cuda, deviceid=0, pix_fmt=\"nv12\"]\n  output1[type=output]  \n  video_input:out_video_url -> videodemuxer:in_video_url\n  videodemuxer:out_video_packet -> videodecoder:in_video_packet\n  videodecoder:out_video_frame -> output1\n}"
+      },
+      "profile": {
+        "dir": "",
+        "profile": false,
+        "trace": false
+      }
+    },
+    {
+      "driver": {
+        "dir": [
+          "/root/ddd/src/flowunit"
+        ],
+        "skip-default": false
+      },
+      "flow": {
+        "desc": "A resize modelbox project"
+      },
+      "graph": {
+        "format": "graphviz",
+        "graphconf": "digraph graph_ddd {\n\tnode [shape=Mrecord]\n  video_input [ type=flowunit flowunit=video_input device=cpu deviceid=\"0\" repeat=\"20\" source_url=\"/xxx/xxx.mp4\" ]\n  videodemuxer [ type=flowunit flowunit=video_demuxer device=cpu deviceid=\"0\" queue_size_event=\"1000\" ] \n  videodecoder [ type=flowunit flowunit=video_decoder device=cuda deviceid=\"0\" pix_fmt=nv12 ]\n  output1 [ type=output ]  \n\n  video_input:out_video_url -> videodemuxer:in_video_url\n  videodemuxer:out_video_packet -> videodecoder:in_video_packet\n  videodecoder:out_video_frame -> output1\n\n}\n"
+      },
+      "profile": {
+        "dir": "/tmp/modelbox/perf/",
+        "profile": false,
+        "session": false,
+        "trace": false
+      }
+    }
+  ],
+  "project_name": "ddd",
+  "project_path": "/root/ddd"
+}
