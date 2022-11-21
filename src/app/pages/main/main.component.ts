f9d0f1f124f6a4755b5d2ad6f342029dc4a91636
ModelBoxDENG <dwjandzzs@gmail.com>
2022-11-21 18:07:32 +0800

    UI: fix port type when lang changed

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -911,10 +911,12 @@ ECHO Port '+ this.portAddress + '>>"%HOMEDRIVE%%HOMEPATH%\\.ssh\\config"\r\n\
     this.hasRedo = !!undoRedoState?.hasRedo;
   };
 
-  isEqual(a, b) {
+  private isEqual(a, b) {
     if (a && b) {
       a = a.replace(/\s+/g, "");
       b = b.replace(/\s+/g, "");
+    } else if (b === undefined) {
+      return true;
     }
     return a === b;
   }
