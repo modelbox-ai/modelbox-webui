0db0c470c92adce1c6fe6ff2f897632add29b5fd
ModelBoxDENG <dwjandzzs@gmail.com>
2022-12-15 11:49:37 +0800

    UI: limit the name of project && add rockchip

--- src/app/components/tool-bar/tool-bar.component.ts
+++ src/app/components/tool-bar/tool-bar.component.ts
@@ -283,7 +283,7 @@ export class ToolBarComponent {
 
   optionsInOut = ['input', 'output'];
   optionsdata_type = ['uint8', 'int', 'int64', 'float', 'float64', 'long', 'double']; //flowunit type
-  optionsdevice = ['cpu', 'cuda', 'ascend'];
+  optionsdevice = ['cpu', 'cuda', 'ascend', 'rockchip'];
   optionsdevicePython = ['cpu'];
   optionsdeviceYolo = ['cpu'];
   flowunitGroupOptions = ['Generic'];
@@ -423,6 +423,7 @@ export class ToolBarComponent {
   out_num = 1;
   point_in = 0;
   point_out = 0;
+  formDataCreateProjectName: string;
 
   tabActiveId: string = "tab1";
   openproject_path: string = this.dataService.defaultSearchPath;
@@ -634,6 +635,19 @@ export class ToolBarComponent {
 
   closeInput() {
     document.getElementById('projectDropDown').click();
+  }
+
+  projectNameOnChange(e) {
+
+    // 只允许项目名称是数字字符和下划线
+    let reg = /^[0-9a-zA-Z_]{1,}$/g;
+    let res = reg.test(e.data);
+
+    if (!res) {
+      e.srcElement.value = e.srcElement.value.substr(0, e.srcElement.value.length - 1);
+      this.formDataCreateProjectName = e.srcElement.value;
+    }
+
   }
 
   handleProjectDropDown(e) {
@@ -1423,8 +1437,6 @@ export class ToolBarComponent {
             } else {
               this.initFormData();
               this.dotSrcEmmitter.emit(this.dataService.defaultSrc);
-              
-              
             }
             this.saveProjectEmmitter.emit();
             let path = this.openproject_path + "/src/graph/" + this.formData.fileName;
